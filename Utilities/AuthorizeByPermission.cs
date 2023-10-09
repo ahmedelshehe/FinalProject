5cd0016e78a6254d5b6898604c4c973ce3916add
ahmedelshehe <ahmedelshehe49@gmail.com>
2023-10-09 17:23:00 +0300

    Error Pages

--- Utilities/AuthorizeByPermission.cs
+++ Utilities/AuthorizeByPermission.cs
@@ -30,8 +30,8 @@ namespace FinalProject.Utilities
 
 			if (!hasPermission)
 			{
-				context.Result = new ForbidResult();
-				return;
+				context.Result = new RedirectToActionResult("Error", "Home", 403);
+                return;
 			}
 		}
 	} 
